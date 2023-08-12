9030f0bfa12e6a51e494179c59649560bd2c0a52
Əhməd Osmanov <96765249+KaidoOP@users.noreply.github.com>
2023-08-13 01:04:38 +0400

    Added CourseDetail Model (One to One Relation)

---
+++ EduHome3/Models/CourseDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EduHome3.Models
+{
+    public class CourseDetail
+    {
+        public int Id { get; set; }
+        public DateTime CourseStart { get; set; }
+        public TimeSpan CourseDuration { get; set; }
+        public TimeSpan ClassDuration { get; set; }
+
+        public int StudentsCount { get; set; }
+        [ForeignKey("Course")]
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
