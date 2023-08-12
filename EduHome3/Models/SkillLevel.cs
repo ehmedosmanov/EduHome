88ff527ca8b2733b81c4c8bd085d6dbffd207b68
Əhməd Osmanov <96765249+KaidoOP@users.noreply.github.com>
2023-08-13 01:05:54 +0400

    Added SkillLevel Model

---
+++ EduHome3/Models/SkillLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace EduHome3.Models
+{
+    public class SkillLevel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Course> Courses { get; set; }
+        public bool IsDeactive { get; set; }
+
+    }
+}
