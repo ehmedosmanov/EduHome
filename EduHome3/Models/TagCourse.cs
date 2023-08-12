a5c9a46e15209468d2ce0e9d1bbde780cf248ba0
Əhməd Osmanov <96765249+KaidoOP@users.noreply.github.com>
2023-08-13 01:04:00 +0400

    TagCourse Model for Many to Many

---
+++ EduHome3/Models/TagCourse.cs
@@ -0,0 +1,12 @@
+﻿namespace EduHome3.Models
+{
+    public class TagCourse
+    {
+        public int Id { get; set; }
+        public Course Course { get; set;}
+        public int CourseId { get; set;}
+
+        public Tag Tag { get; set; }
+        public int TagId { get; set; }
+    }
+}
