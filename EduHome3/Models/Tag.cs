363e01a9a7b20ece33fcea0012ccbeaabbfb0d03
Əhməd Osmanov <96765249+KaidoOP@users.noreply.github.com>
2023-08-13 01:04:59 +0400

    Added Tag Model

---
+++ EduHome3/Models/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace EduHome3.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeactive { get; set; }
+        public List<TagCourse> TagCourses { get; set; }
+
+    }
+}
