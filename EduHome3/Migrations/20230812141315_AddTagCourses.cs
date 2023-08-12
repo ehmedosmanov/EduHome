016b07bf820462b0c1b64dcebc7e7181c6ccd614
Əhməd Osmanov <96765249+KaidoOP@users.noreply.github.com>
2023-08-13 01:03:38 +0400

    All Relations(One to Many, Many to Many, One to One) Migrations

---
+++ EduHome3/Migrations/20230812141315_AddTagCourses.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EduHome3.Migrations
+{
+    public partial class AddTagCourses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LanguageId",
+                table: "Courses",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Courses",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SkillLevelId",
+                table: "Courses",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "CourseDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseStart = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CourseDuration = table.Column<TimeSpan>(type: "time", nullable: false),
+                    ClassDuration = table.Column<TimeSpan>(type: "time", nullable: false),
+                    StudentsCount = table.Column<int>(type: "int", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CourseDetails_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeactive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SkillLevels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeactive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SkillLevels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeactive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TagCourses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseId = table.Column<int>(type: "int", nullable: false),
+                    TagId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TagCourses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TagCourses_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TagCourses_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_LanguageId",
+                table: "Courses",
+                column: "LanguageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_SkillLevelId",
+                table: "Courses",
+                column: "SkillLevelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseDetails_CourseId",
+                table: "CourseDetails",
+                column: "CourseId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TagCourses_CourseId",
+                table: "TagCourses",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TagCourses_TagId",
+                table: "TagCourses",
+                column: "TagId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Languages_LanguageId",
+                table: "Courses",
+                column: "LanguageId",
+                principalTable: "Languages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_SkillLevels_SkillLevelId",
+                table: "Courses",
+                column: "SkillLevelId",
+                principalTable: "SkillLevels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Languages_LanguageId",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_SkillLevels_SkillLevelId",
+                table: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "CourseDetails");
+
+            migrationBuilder.DropTable(
+                name: "Languages");
+
+            migrationBuilder.DropTable(
+                name: "SkillLevels");
+
+            migrationBuilder.DropTable(
+                name: "TagCourses");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_LanguageId",
+                table: "Courses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_SkillLevelId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "LanguageId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "SkillLevelId",
+                table: "Courses");
+        }
+    }
+}
---
+++ EduHome3/Migrations/20230812142548_AddCategoryTableWithRelationCourse.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EduHome3.Migrations
+{
+    public partial class AddCategoryTableWithRelationCourse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Courses",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoryCount = table.Column<int>(type: "int", nullable: false),
+                    IsDeactive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_CategoryId",
+                table: "Courses",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Category_CategoryId",
+                table: "Courses",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Category_CategoryId",
+                table: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_CategoryId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Courses");
+        }
+    }
+}
