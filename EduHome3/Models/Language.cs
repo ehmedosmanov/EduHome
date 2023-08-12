77c95d04ab663b5400147323f5259dabe4176aa2
Əhməd Osmanov <96765249+KaidoOP@users.noreply.github.com>
2023-08-13 01:06:37 +0400

    Added Language Model

---
+++ EduHome3/Models/Language.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace EduHome3.Models
+{
+    public class Language
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Course> Courses { get; set; } 
+        public bool IsDeactive { get; set; }
+    }
+}
