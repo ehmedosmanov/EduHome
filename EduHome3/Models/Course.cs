2edf8da70b28873798960aca6f3fdca1dd551779
Əhməd Osmanov <96765249+KaidoOP@users.noreply.github.com>
2023-08-13 01:05:32 +0400

    Changed course model

--- EduHome3/Models/Course.cs
+++ EduHome3/Models/Course.cs
@@ -1,4 +1,6 @@
-﻿namespace EduHome3.Models
+﻿using System.Collections.Generic;
+
+namespace EduHome3.Models
 {
     public class Course
     {
@@ -6,6 +8,21 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string Img { get; set; }
+        public decimal Price { get; set; }
+
+        public SkillLevel SkillLevel { get; set; }
+        public int? SkillLevelId { get; set; }
+
+        public Language Language { get; set; }
+        public int? LanguageId { get; set; }
+
+        public List<TagCourse> TagCourses { get; set; }
         public bool IsDeactive { get; set; }
+
+        public CourseDetail CourseDetail { get; set; }
+
+        public int? CategoryId { get; set; }
+        public Category Category { get; set; }
+
     }
 }
